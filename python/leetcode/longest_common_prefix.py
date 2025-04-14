03daf81e371a37076cff9152d3f08de02978e150
nganouwg <98375015+nganouwg@users.noreply.github.com>
2025-04-14 09:33:21 -0500

    Add step by step instructions for the algorithms

--- python/leetcode/longest_common_prefix.py
+++ python/leetcode/longest_common_prefix.py
@@ -27,14 +27,36 @@ Constraints:
 
 '''
 
-#HINT: Count the number of time each letter appears for a given 
+'''
+    Solution:
+
+    1. Determine the lenght of the shortest_word
+
+    2. Set an index-i ranging from 0 to the size of the shorted_word - 1 to compare characters by positipn
+
+    3. set the cur_letter to an empty string, which is used to hold prefix character at index-i.
+
+    4. Loop through each word at the same index
+
+    5. if the cur_letter is empty, this means we are at a new character position, 
+       so set the cur_letter to the first word's character at index-i 
+
+    6. Compare the cur_letter to the same position characters for subsequent words. 
+    
+    7. If a mismatch is found set end_of_prefix to true, 
+       which mean we no longer found characters for the common prefix and we can break out of the loops
+
+    8. If no mismatch is found include the cur_letter to the prefix
+
+    9. Display the common prefix
+'''
 
 
 def run(strs):
 
     prefixes = ""
     end_of_prefix = False
-    prev_letter = ""
+    cur_letter = ""
     shortest_word_len = len(strs[0])
 
     for word in strs:
@@ -43,17 +65,17 @@ def run(strs):
 
     i = 0
     while(i < shortest_word_len):
-        prev_letter = ""
+        cur_letter = ""
         for word in strs:
-            if prev_letter == "":
-                prev_letter = word[i]
+            if cur_letter == "":
+                cur_letter = word[i]
             else:
-                if prev_letter != word[i]:
+                if cur_letter != word[i]:
                     end_of_prefix = True
                     break
 
         if end_of_prefix == False:
-            prefixes = prefixes + prev_letter
+            prefixes = prefixes + cur_letter
         else:
             break
 
